588eff3c4dddaa3c5e2003c7827052df0b103152
Junit38 <Junit38@gmail.com>
2015-08-30 17:57:42 +0200

    Merge MeanJs 4.4

--- modules/core/client/app/config.js
+++ modules/core/client/app/config.js
@@ -7,6 +7,7 @@ var ApplicationConfiguration = (function () {
   var applicationModuleVendorDependencies = [
     'ngResource',
     'ngAnimate',
+    'ngMessages',
     'ui.router',
     'ui.bootstrap',
     'ui.utils',
--- modules/users/server/config/strategies/facebook.js
+++ modules/users/server/config/strategies/facebook.js
@@ -17,6 +17,18 @@ module.exports = function (config) {
       passReqToCallback: true
     },
     function (req, accessToken, refreshToken, profile, done) {
+      function generateUsername(profile) {
+        var username = '';
+
+        if (profile.emails) {
+          username = profile.emails[0].value.split('@')[0];
+        } else if (profile.name) {
+          username = profile.name.givenName[0] + profile.name.familyName;
+        }
+
+        return username.toLowerCase() || undefined;
+      }
+      
       // Set the provider data and include tokens
       var providerData = profile._json;
       providerData.accessToken = accessToken;
@@ -27,7 +39,8 @@ module.exports = function (config) {
         firstName: profile.name.givenName,
         lastName: profile.name.familyName,
         displayName: profile.displayName,
-        email: profile.emails[0].value,
+        email: profile.emails ? profile.emails[0].value : undefined,
+        username: profile.username || generateUsername(profile),
         profileImageURL: (profile.id) ? '//graph.facebook.com/' + profile.id + '/picture?type=large' : undefined,
         provider: 'facebook',
         providerIdentifierField: 'id',
